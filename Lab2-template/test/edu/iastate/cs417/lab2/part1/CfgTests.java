59d86eff07fd38f33adf3191fb7c45d0b539f12d
anniesteenson <anniesteenso@gmail.com>
2017-09-18 21:28:30 -0500

    Added lab 2 template

---
+++ Lab2-template/test/edu/iastate/cs417/lab2/part1/CfgTests.java
@@ -0,0 +1,14 @@
+package edu.iastate.cs417.lab2.part1;
+
+
+/**
+ * 
+ * CFG derived (Part I) tests go in this class. The tests should be simple unit tests. 
+ * One test method per test case; all in this class. 
+ * 
+ * @author 
+ *
+ */
+public class CfgTests {
+
+}
---
+++ Lab2-template/test/edu/iastate/cs417/lab2/part2/PartIITests.java
@@ -0,0 +1,18 @@
+package edu.iastate.cs417.lab2.part2;
+
+
+
+/**
+ * The parameterized test mechanism for Part II goes in this class. 
+ * 
+ * Do not try to get the name of the test set file from the keyboard. Remember, this
+ * is an automated test suite; it needs to run without user intervention. Thus, the file name
+ * needs to always be known to the test. 
+ * 
+ * @author 
+ *
+ */
+public class PartIITests {
+   private static final String filename = "testdata.txt";
+   
+}
